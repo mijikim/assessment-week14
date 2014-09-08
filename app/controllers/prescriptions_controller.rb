dae88c8fa61ef1c1946ba60b9188adb2b1f012f5
mijikim <miji.kim926@gmail.com>
2014-09-08 15:32:16 -0600

    user can see error messages when creating a prescription with data missing

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -8,16 +8,28 @@ class PrescriptionsController < ApplicationController
 
   def create
     @patient = Patient.find(params[:patient_id])
-    @prescription = Prescription.new(params.fetch(:prescription, {}).permit(:medication_id, :start_date, :end_date, :dosage, :schedule))
+    @prescription = Prescription.new(allowed_params)
     @prescription.patient = @patient
     if @prescription.save
-      flash[:notice] = "Prescription was created successfully!"
+      flash[:notice] = "Your prescription has been created"
       redirect_to patient_path(params[:patient_id])
     else
-      flash[:error] = @prescription.errors.full_messages.each do |message|
-        message
-      end
+      @medications = Medication.all
+      render_error_messages(@prescription)
       render :new
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.fetch(:prescription, {}).permit(:medication_id, :start_date, :end_date, :dosage, :schedule)
+  end
+
+  def render_error_messages(prescription)
+    # flash[:error] = []
+    prescription.errors.full_messages.each do |message|
+    flash[:error] = message
     end
   end
 
--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,8 +1,8 @@
 class Prescription < ActiveRecord::Base
 
+  validates_presence_of :dosage, :schedule
+
   belongs_to :medication
   belongs_to :patient
-
-  validates_presence_of :medication, :patient, :dosage, :schedule
 
 end
