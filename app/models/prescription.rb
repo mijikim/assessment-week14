3ff93e22cbd15592e6da99dd5a35a0dca3d64cac
mijikim <miji.kim926@gmail.com>
2014-09-08 16:33:52 -0600

    Dosage must start with a number error msg added

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,6 +1,7 @@
 class Prescription < ActiveRecord::Base
 
   validates_presence_of :dosage, :schedule
+  validates_format_of :dosage, :with => /\A[1-9]/, :message => " must start with a number"
 
   belongs_to :medication
   belongs_to :patient
